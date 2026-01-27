68d88b86ed2ba9a7b350d561cf65ef09e2a42657
Franoll Fantu <fantu.franoll1@gmail.com>
2026-01-27 13:55:44 -0800

    Phase 2 Finished

--- backend/main.py
+++ backend/main.py
@@ -1,14 +1,47 @@
 # Surge Main.py
 # this creates the web server
 # fastapi will handle the routing + HTTP requests + responses
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 # fastapi.responses lets us return file-like-data
 from fastapi.responses import StreamingResponse
-
 from storage import create_surge_id
 from qr import generate_qr_code
+from datetime import datetime
+# redis is a temporary system memory
+# that we will use to store the Active SURGE_IDS and Scan Event Lists
+import redis
+import json
+from pydantic import BaseModel
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+
+class ScanRequest(BaseModel):
+    surge_id: str
+    zone: str
+
 
 app = FastAPI(title="SURGE")
+
+REDIS_HOST = os.getenv("REDIS_HOST")
+REDIS_PORT = os.getenv("REDIS_PORT")
+r = redis.Redis(
+    host=REDIS_HOST,
+    port=REDIS_PORT,
+    decode_responses=True,
+    socket_connect_timeout=2,
+    socket_timeout=2
+)
+
+VALID_ZONES = {
+    "terminal_entry",
+    "security",
+    "customs",
+    "boarding_gate",
+    "transfer",
+    "amenities"
+}
 
 
 @app.get("/issue")
@@ -17,9 +50,49 @@ def issue_surge_id():
 
     surge = create_surge_id()
 
+    r.set(
+        f"surge:{surge.id}",
+        "active",
+        ex=3600
+    )
+
     qr_buffer = generate_qr_code(str(surge.id))
+    print("Issued SURGE ID:", surge.id)
 
     return StreamingResponse(
         qr_buffer,
         media_type="image/png"
     )
+
+
+@app.post("/scan")
+def scanqrcode(data: ScanRequest):
+    surge_id = data.surge_id
+    zone = data.zone
+    # first we need to check if the surgeid even exists
+    if not r.exists(f"surge:{surge_id}"):
+        raise HTTPException(
+            status_code=404, detail="Invalid or expired SURGE ID")
+
+    # second validate the zone
+    if zone not in VALID_ZONES:
+        raise HTTPException(status_code=400, detail="Invalid Zone")
+
+    # third we need to time when they scan
+
+    timestamp = datetime.utcnow().isoformat()
+
+    # fourth we need to store that scanne event
+    scan_event = {
+        "zone": zone,
+        "timestamp": timestamp
+    }
+
+    r.rpush(f"surge:{surge_id}:scans", json.dumps(scan_event))
+
+    return {
+        "status": "scan recorded",
+        "surge_id": surge_id,
+        "zone": zone,
+        "timestamp": timestamp
+    }
